67ae2eb9263b8b12237800eaf9d75457b2deca81
vitekambroz <vitek.ambroz1@email.cz>
2025-12-10 11:17:50 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,9 +3,9 @@ root = tk.Tk()
 root.geometry("300x300")
 root.resizable(False, False)
 root.title("Generator")
-
-tk.Entry(root).grid(column=0, row=0)
-tk.Button(root, text="Přidat").grid(column=1, row=0)
+tk.Label(root, text="Generátor Vánočních přání").grid(row=0, column=0)
+tk.Entry(root).grid(column=0, row=1)
+tk.Button(root, text="Přidat").grid(column=1, row=1)
 
 
 root.mainloop()
