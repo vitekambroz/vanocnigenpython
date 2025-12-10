df2f913eafbace5cc93da9a600267e300a00d660
ministrvojtech <ministrvojtech@gmail.com>
2025-12-10 11:19:53 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,8 +3,11 @@ root = tk.Tk()
 root.geometry("300x300")
 root.resizable(False, False)
 root.title("Generator")
+
 tk.Label(root, text="Generátor Vánočních přání").grid(row=0, column=0)
+
 tk.Entry(root).grid(column=0, row=1)
+
 tk.Button(root, text="Přidat").grid(column=1, row=1)
 
 
